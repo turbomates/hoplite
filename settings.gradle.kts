c796e2ea9e55f34febe475414b7e6354ec843351
Vadim Golodko <vadim@turbomates.com>
2024-06-14 10:18:51 +0200

    up version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,17 @@
 rootProject.name = "hoplite"
 
-
 dependencyResolutionManagement {
     versionCatalogs {
         create("deps") {
             version("ktor", "2.3.11")
-            version("detekt", "1.21.0")
+            version("detekt", "1.23.6")
             version("hoplite", "2.7.5")
             version("kotlin", "2.0.0")
             version("secretsmanager", "1.1.0")
             version("test_logger", "3.0.0")
             version("kotlin_serialization_json", "1.7.0")
             version("nexus_staging", "0.30.0")
-            version("mockito-kotlin", "4.0.0")
+            version("mockito-kotlin", "5.3.1")
 
             library("ktor_client_content_negotiation", "io.ktor", "ktor-client-content-negotiation").versionRef("ktor")
             library("ktor_client_cio", "io.ktor", "ktor-client-cio").versionRef("ktor")
